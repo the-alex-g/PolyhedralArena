8ad741c7ea81468f5184da082557f50697a67982
the-alex-g <alex.gestwicki@gmail.com>
2022-07-16 11:33:09 -0400

    Add custom audio scene

---
+++ project/Effects/VariableAudio.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer3D
+
+export var delay := 0.0
+
+func _ready()->void:
+	yield(get_tree().create_timer(delay), "timeout")
+	unit_db += randf()
+	pitch_scale += randf() - 0.5
+	play()
