5bc84c281abef73b59beb93d86268bfe590cac7c
the-alex-g <alex.gestwicki@gmail.com>
2022-07-16 14:34:39 -0400

    Add cool gates for spawning

--- project/Main/Spawner.gd
+++ project/Main/Spawner.gd
@@ -1,15 +1,21 @@
-extends Position3D
+extends Spatial
 
 var _onscreen := false
+var _player
 
 func spawn(player:Player)->void:
 	if _onscreen:
-		var enemy = load("res://Dice/EnemyDie.tscn").instance() as Enemy
-		enemy.target = player
-		enemy.translation = translation
-		enemy.translation.y += 1.73
-		get_parent().get_parent().add_child(enemy)
-		enemy.set_deferred("level", (randi() % 6) + 1)
+		_player = player
+		$AnimationPlayer.play("Spawn")
+
+
+func _create_enemy()->void:
+	var enemy = load("res://Dice/EnemyDie.tscn").instance() as Enemy
+	enemy.target = _player
+	enemy.translation = translation
+	enemy.translation.y += 1.73
+	get_parent().get_parent().add_child(enemy)
+	enemy.set_deferred("level", (randi() % 6) + 1)
 
 
 func _on_VisibilityNotifier_camera_entered(_camera)->void:
