441556dd74224a3d0af2a2051fec0f087c750da5
Alex G <alex.gestwicki@gmail.com>
2024-03-27 09:25:03 -0400

    Add simple escalation

--- project/Main/Spawner.gd
+++ project/Main/Spawner.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 var _onscreen := false
-var _player
+var _player : Player
+var _level := 1
+
 
 func spawn(player:Player)->void:
 	if _onscreen:
@@ -10,12 +12,23 @@ func spawn(player:Player)->void:
 
 
 func _create_enemy()->void:
-	var enemy = load("res://Dice/EnemyDie.tscn").instantiate() as Enemy
-	enemy.target = _player
-	enemy.position = position
-	enemy.position.y += 1.73
-	get_parent().get_parent().add_child(enemy)
-	enemy.set_deferred("level", (randi() % 6) + 1)
+	var enemy_levels := [randi_range(1, 6)]
+	var total_enemy_levels : int = enemy_levels[0]
+	while total_enemy_levels < _level:
+		var enemy_level := randi_range(1, 6)
+		if total_enemy_levels + enemy_level <= _level:
+			enemy_levels.append(enemy_level)
+			total_enemy_levels += enemy_level
+		else:
+			break
+	for enemy_level in enemy_levels:
+		var enemy = load("res://Dice/EnemyDie.tscn").instantiate() as Enemy
+		enemy.target = _player
+		enemy.position = position
+		enemy.position.y += 1.73
+		get_parent().get_parent().add_child(enemy)
+		enemy.set_deferred("level", enemy_level)
+		await get_tree().create_timer(0.2).timeout
 
 
 func _on_visible_on_screen_notifier_3d_screen_entered()->void:
@@ -24,3 +37,7 @@ func _on_visible_on_screen_notifier_3d_screen_entered()->void:
 
 func _on_visible_on_screen_notifier_3d_screen_exited()->void:
 	_onscreen = false
+
+
+func _on_level_up_timer_timeout()->void:
+	_level += 1
